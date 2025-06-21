4bdc9f569fbcb99a5bcb2c0244d71686189292d9
AndyStar <sbandystar@gmail.com>
2025-06-21 11:47:43 +0800

    Pos3d class changed to be a subclass of Vector3d

--- Pos3d.cpp
+++ Pos3d.cpp
@@ -7,6 +7,13 @@ std::string Pos3d::to_string() const {
     return "Pos3d( x:" + std::to_string(x) + ", y:" + std::to_string(y) + ", z:" + std::to_string(z) + " )";
 }
 
+double Pos3d::distance_to(const Pos3d& other) const {
+    return sqrt(
+        (x - other.x) * (x - other.x) +
+        (y - other.y) * (y - other.y) +
+        (z - other.z) * (z - other.z)
+    );
+}
 
 // operators
 Pos3d Pos3d::operator+(const Vector3d& vect) const {
@@ -14,6 +21,9 @@ Pos3d Pos3d::operator+(const Vector3d& vect) const {
 }
 Pos3d Pos3d::operator-(const Vector3d& vect) const {
     return Pos3d(x-vect.x, y-vect.y, z-vect.z);
+}
+Vector3d Pos3d::operator-(const Pos3d& other) const {
+    return Vector3d(x - other.x, y - other.y, z - other.z);
 }
 bool Pos3d::operator==(const Pos3d& other) const {
     return (x == other.x && y == other.y && z == other.z);
--- Pos3d.hpp
+++ Pos3d.hpp
@@ -4,13 +4,10 @@
 #include <string>
 #include "Vector3d.hpp"
 
-class Pos3d {
+class Pos3d : public Vector3d{
     public:
-        int x;
-        int y;
-        int z;
-        inline explicit Pos3d(int arg_x, int arg_y, int arg_z) 
-            : x(arg_x), y(arg_y), z(arg_z)
+        inline explicit Pos3d(const double& arg_x, const double& arg_y, const double& arg_z)
+            : Vector3d(arg_x, arg_y, arg_z)
         {}
         inline Pos3d(const Pos3d&) = default;
         inline Pos3d(Pos3d&&) = default;
@@ -28,8 +25,20 @@ class Pos3d {
         
         std::string to_string() const;
 
+
+        double distance_to(const Pos3d& other) const;
+
+
+        // Operators
+
         Pos3d operator+(const Vector3d& vect) const;
         Pos3d operator-(const Vector3d& vect) const;
+        Vector3d operator-(const Pos3d& other) const;
+
+        // Deleted operators
+        inline double dot_mul(const Vector3d& other) const = delete;
+        inline Vector3d cross_mul(const Vector3d& other) const = delete;
+
 
         bool operator==(const Pos3d& other) const;
 };
