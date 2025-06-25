da32da395ea22ef3c7a9bf218924404559a6c6c7
AndyStar <sbandystar@gmail.com>
2025-06-25 17:44:31 +0800

    branch-test_shape_use_basis add edit and more construct functions for class Basis3d

--- Basis3d.cpp
+++ Basis3d.cpp
@@ -1,15 +1,72 @@
 #include "Basis3d.hpp"
 
-#include "stdexcept"
+#include <stdexcept>
+
 
 
 Basis3d::Basis3d(const std::vector<std::vector<double>>& arg_data)
     : Matrix(arg_data, false) {
-    throw_if_not_3x3<std::invalid_argument>(); 
+    throw_if_not_3x3<std::invalid_argument>("Basis3d");
+    throw_if_basis_not_perpendicular<std::invalid_argument>("Basis3d");
 }
-Basis3d::Basis3d(size_t arg_num_of_row, size_t arg_num_of_col, double default_val)
-    : Matrix(arg_num_of_row, arg_num_of_col, default_val, false) {
-    throw_if_not_3x3<std::invalid_argument>();
+Basis3d::Basis3d(const Vector3d& x, const Vector3d& y, const Vector3d& z)
+    : Matrix(3, 3, 0, false) {
+    bool x_is_zero = x.is_zero_vector();
+    bool y_is_zero = y.is_zero_vector();
+    bool z_is_zero = z.is_zero_vector();
+    
+    if ((x_is_zero && y_is_zero) 
+        || (y_is_zero && z_is_zero) 
+        || (z_is_zero && x_is_zero)
+    ) {
+        throw std::invalid_argument("Basis3d::Basis3d InvalidArg: Basis3d cannot be created with only 1 axis vector provided");
+    }
+    
+    if (z_is_zero) { set_basis_xy(x, y); return; }
+    if (x_is_zero) { set_basis_yz(y, z); return; }
+    if (y_is_zero) { set_basis_zx(z, x); return; }
+
+    set_basis(x, y, z);
+}
+Basis3d::Basis3d() 
+    : Matrix(3, 3, 0) {
+    for (unsigned int i = 0; i < 3; ++i) {
+        operator[](i)[i] = 1;
+    }
+}
+
+void Basis3d::set_basis_xy(const Vector3d& x, const Vector3d& y) {
+    throw_if_not_perpendicular<std::invalid_argument>(x, y, "set_basis");
+    set_basis_no_check(
+        x.unit_vector(), 
+        y.unit_vector(), 
+        x.cross_mul(y).unit_vector()
+    );
+}
+void Basis3d::set_basis_yz(const Vector3d& y, const Vector3d& z) {
+    throw_if_not_perpendicular<std::invalid_argument>(y, z, "set_basis");
+    set_basis_no_check(
+        y.cross_mul(z).unit_vector(), 
+        y.unit_vector(), 
+        z.unit_vector()
+    );
+}
+void Basis3d::set_basis_zx(const Vector3d& z, const Vector3d& x) {
+    throw_if_not_perpendicular<std::invalid_argument>(z, x, "set_basis");
+    set_basis_no_check(
+        x.unit_vector(), 
+        z.cross_mul(x).unit_vector(), 
+        z.unit_vector()
+    );
+}
+
+void Basis3d::set_basis(const Vector3d& x, const Vector3d& y, const Vector3d& z) {
+    throw_if_not_perpendicular<std::invalid_argument>(x, y, z, "set_basis");
+    set_basis_no_check(
+        x.unit_vector(),
+        y.unit_vector(),
+        z.unit_vector()
+    );
 }
 
 Vector3d Basis3d::local_to_world(const Vector2d& vect) const {
@@ -17,4 +74,11 @@ Vector3d Basis3d::local_to_world(const Vector2d& vect) const {
 }
 Vector3d Basis3d::local_to_world(const Vector3d& vect) const {
     return x_axis()*vect.x + y_axis()*vect.y + z_axis()*vect.z;
+}
+
+void Basis3d::set_basis_no_check(const Vector3d& x, const Vector3d& y, const Vector3d& z) {for (unsigned int i = 0; i < 3; ++i) {
+        operator[](i)[0] = x[i];
+        operator[](i)[1] = y[i];
+        operator[](i)[2] = z[i];
+    }
 }
--- Basis3d.hpp
+++ Basis3d.hpp
@@ -1,6 +1,7 @@
 #ifndef BASIS_3D_HPP
 #define BASIS_3D_HPP
 
+#include <string>
 #include <stdexcept>
 
 #include "Matrix.hpp"
@@ -11,14 +12,33 @@ class Basis3d : public Matrix {
     
     public:
         explicit Basis3d(const std::vector<std::vector<double>>& arg_data);
-        explicit Basis3d(size_t arg_num_of_row, size_t arg_num_of_col, double default_val = 0.0);
+        explicit Basis3d(const Vector3d& x = Vector3d(), const Vector3d& y = Vector3d(), const Vector3d& z = Vector3d());
+        explicit Basis3d(); // returns an identity basis
+
+        void set_basis_xy(const Vector3d& x, const Vector3d& y);
+        void set_basis_yz(const Vector3d& y, const Vector3d& z);
+        void set_basis_zx(const Vector3d& z, const Vector3d& x);
+        void set_basis(const Vector3d& x, const Vector3d& y, const Vector3d& z);
+
+        
         inline Vector3d x_axis() const { return get_column(0); }
         inline Vector3d y_axis() const { return get_column(1); }
         inline Vector3d z_axis() const { return get_column(2); }
         Vector3d local_to_world(const Vector2d& vect) const;
         Vector3d local_to_world(const Vector3d& vect) const;
 
+        
+
+
     private:
+        void set_basis_no_check(const Vector3d& x, const Vector3d& y, const Vector3d& z);
+        template<typename T>
+        inline void assert_type_is_exception() const {
+            static_assert(
+                std::is_base_of<std::exception, T>::value, 
+                "type must derive from std::exception"
+            );
+        }
         /**
          * @brief Throws an exception if the matrix is not 3x3.
          * 
@@ -32,13 +52,48 @@ class Basis3d : public Matrix {
          * @throws ExceptionType if the matrix is not 3x3.
          */
         template<typename ExceptionType = std::runtime_error>
-        void throw_if_not_3x3() const {
-            static_assert(
-                std::is_base_of<std::exception, ExceptionType>::value, 
-                "ExceptionType must derive from std::exception"
-            );
+        inline void throw_if_not_3x3(std::string func_name) const {
+            assert_type_is_exception<ExceptionType>();
             if (!is_size_equal(3, 3)) {
-                throw std::ExceptionType("Basis3d::throw_if_invalid_size InvalidArg: size of matrix for basis3d must be 3x3");
+                throw ExceptionType("Basis3d::" + func_name + " throw_if_not_3x3: size of matrix for basis3d must be 3x3");
+            }
+        }
+        template<typename ExceptionType = std::runtime_error>
+        inline void throw_if_basis_not_perpendicular(std::string func_name) const {
+            assert_type_is_exception<ExceptionType>();
+            if (
+                !x_axis().is_perpendicular(y_axis()) ||
+                !y_axis().is_perpendicular(z_axis()) ||
+                !z_axis().is_perpendicular(x_axis())
+            ) {
+                throw ExceptionType(
+                    "Basis3d::" + func_name 
+                    + " throw_if_basis_not_perpendicular: basis has to be perpendicular with each other"
+                );
+            }
+        }
+        template<typename ExceptionType = std::runtime_error>
+        inline void throw_if_not_perpendicular(const Vector3d& x, const Vector3d& y, std::string func_name) const {
+            assert_type_is_exception<ExceptionType>();
+            if (!x_axis().is_perpendicular(y_axis())) {
+                throw ExceptionType(
+                    "Basis::" + func_name + " throw_if_not_perpendicular vect1:"
+                    + x.to_string(false) + " vect2:" + y.to_string(false));
+            }
+        }
+        template<typename ExceptionType = std::runtime_error>
+        inline void throw_if_not_perpendicular(const Vector3d& x, const Vector3d& y, const Vector3d& z, std::string func_name) const {
+            assert_type_is_exception<ExceptionType>();
+            if (
+                !x_axis().is_perpendicular(y_axis()) ||
+                !y_axis().is_perpendicular(z_axis()) ||
+                !z_axis().is_perpendicular(x_axis())
+            ) {
+                throw ExceptionType(
+                    "Basis3d::" + func_name 
+                    + " throw_if_not_perpendicular: x:" + x.to_string(false) 
+                    + " y:" + y.to_string(false) + " z:" + z.to_string(false)
+                );
             }
         }
 };
